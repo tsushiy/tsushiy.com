e6c485db56affd846a7cdbccd07f7e18825e2234
tsushiy <28962399+tsushiy@users.noreply.github.com>
2022-01-27 00:33:53 +0900

    Remove facebook share icon

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,14 +1,6 @@
 import { styled } from '@mui/system'
 import React from 'react'
-import {
-  FacebookShareButton,
-  PocketShareButton,
-  TwitterShareButton,
-  FacebookShareCount,
-  FacebookIcon,
-  PocketIcon,
-  TwitterIcon
-} from 'react-share'
+import { PocketShareButton, TwitterShareButton, PocketIcon, TwitterIcon } from 'react-share'
 import urljoin from 'url-join'
 
 import { PostNode } from 'types/markdown-node'
@@ -38,18 +30,12 @@ const SocialLinks: FC<Props> = (props) => {
   const post = postNode.frontmatter
   const url = urljoin(config.siteUrl, config.pathPrefix, postPath)
   const iconSize = mobile ? 36 : 48
-  const filter = (count: number) => (count > 0 ? count : '')
-  const renderShareCount = (count: number) => <div className="share-count">{filter(count)}</div>
 
   return (
     <SocialLinksContainer>
       <TwitterShareButton url={url} title={post.title}>
         <TwitterIcon round size={iconSize} />
       </TwitterShareButton>
-      <FacebookShareButton url={url} quote={postNode.excerpt}>
-        <FacebookIcon round size={iconSize} />
-        <FacebookShareCount url={url}>{(count) => renderShareCount(count)}</FacebookShareCount>
-      </FacebookShareButton>
       <PocketShareButton url={url} title={post.title}>
         <PocketIcon round size={iconSize} />
       </PocketShareButton>
